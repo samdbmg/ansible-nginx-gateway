62c1a55006e7d7f2e2e378f37988bc57b47345c4
Sam Mesterton-Gibbons <sam@samn.co.uk>
2020-12-30 12:47:41 +0000

    Expand private key in tests
    Fixes a problem where tests would fail on focal because the certificate
    key is too short.

--- molecule/default/resources/make-fake-cert.sh
+++ molecule/default/resources/make-fake-cert.sh
@@ -9,11 +9,11 @@ mkdir generated-certs
     cd generated-certs
 
     # Generate root key and CA
-    openssl genrsa -out fake-client-ca.key 1024
-    openssl req -x509 -new -nodes -key fake-client-ca.key -sha256 -days 1024 -subj "/C=GB/ST=Manchester/O=Sample/CN=fake-client-cert." -out fake-client-ca.crt
+    openssl genrsa -out fake-client-ca.key 2048
+    openssl req -x509 -new -nodes -key fake-client-ca.key -sha256 -days 2048 -subj "/C=GB/ST=Manchester/O=Sample/CN=fake-client-cert." -out fake-client-ca.crt
 
     # Generate key for cert
-    openssl genrsa -out fake-client-cert.key 1024
+    openssl genrsa -out fake-client-cert.key 2048
 
     # Generate CSR
     openssl req -new -sha256 -key fake-client-cert.key -subj "/C=GB/ST=Manchester/O=Sample/CN=user@fake-client-cert." -out fake-client-cert.csr
